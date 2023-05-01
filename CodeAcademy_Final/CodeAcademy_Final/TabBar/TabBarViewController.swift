909791dfdabfbaab50dc651d704a9b4d683f07a6
Egidijus <vaitkeviciusegis@gmail.com>
2023-05-01 07:29:08 +0300

    TabBarVC refactored

--- CodeAcademy_Final/CodeAcademy_Final/TabBar/TabBarViewController.swift
+++ CodeAcademy_Final/CodeAcademy_Final/TabBar/TabBarViewController.swift
@@ -34,6 +34,7 @@ class TabBarViewController: UITabBarController {
         let transactionsViewControllerNavigation = UINavigationController(rootViewController: transactionsVC)
         setViewControllers([homeVC, transactionsViewControllerNavigation, transferVC, settingsVC], animated: true)
         guard let items = tabBar.items else { return }
+        
         let images = ["house.fill", "arrow.left.arrow.right","list.bullet.clipboard.fill", "gearshape.fill"]
         let color = UIColor(red: 42/255, green: 175/255, blue: 134/255, alpha: 1)
         let titleColor = UIColor(red: 42/255, green: 175/255, blue: 134/255, alpha: 1)
@@ -58,40 +59,40 @@ class TabBarViewController: UITabBarController {
         guard let serviceAPI = serviceAPI else {
             return
         }
-        // Create a new AccountEntity instance
+        
         guard let container = CoreDataManager.sharedInstance.container?.viewContext else {
             return
         }
         
         let loggedInAccount = AccountEntity(context: container)
         
-        // Set the properties of the AccountEntity instance
         loggedInAccount.id = Int64(loggedInUser.accountInfo.id)
         
-        // Save the changes to Core Data
         CoreDataManager.sharedInstance.saveAccountToCoreData(accountEntity: loggedInAccount)
-        serviceAPI.fetchingTransactions(url: URLBuilder.getTransactionURL(withId: loggedInUser.accountInfo.id), completion: { (result) in
+        serviceAPI.fetchingTransactions(url: URLBuilder.getTransactionURL(withId: loggedInUser.accountInfo.id), completion: { [weak self] (result) in
+            guard self == self else {
+                return
+            }
             DispatchQueue.main.async {
                 switch result {
                     case .success(_):
-             break
+                        break
                     case .failure(let error):
                         print("Error processing json data: \(error)")
                 }
             }
-            
         })
         transactionsVC.currentLoggedInAccount = loggedInAccount
+        transactionsVC.viewModel = viewModel
         homeVC.serviceAPI = serviceAPI
+        homeVC.viewModel = viewModel
+        homeVC.loggedInUser = loggedInUser
         settingsVC.serviceAPI = serviceAPI
         settingsVC.homeVC = homeVC
-        transferVC.serviceAPI = serviceAPI
-        transactionsVC.viewModel = viewModel
-        homeVC.viewModel = viewModel
-        transferVC.viewModel = viewModel
-        homeVC.loggedInUser = loggedInUser
         settingsVC.loggedInUser = loggedInUser
         transferVC.loggedInUser = loggedInUser
+        transferVC.viewModel = viewModel
+        transferVC.serviceAPI = serviceAPI
     }
     
 }
